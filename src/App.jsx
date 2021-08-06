18e0c7d5ce6d4c588c7017175d9bb80529384d3b
dennis <dennismarcelo15@gmail.com>
2021-08-06 11:19:03 -0300

    add routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import HardSkills from './pages/HardSkills';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <header>Aqui vai o header</header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/hardskills" component={HardSkills} />
+          <Route exact path="/projects" component={Projects} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
