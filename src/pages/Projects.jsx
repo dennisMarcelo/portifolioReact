28e20271a8b450d543ac01db50a8b084546ad263
dennisMarcelo <dennismarcelo15@gmail.com>
2021-08-26 10:40:02 -0300

    page project complete

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../provider/Context';
+import './style/Projects.css';
 
 function Projects() {
+  const { dataUser: { projects } } = useContext(Context);
+
+  const cards = () => (
+    <>
+      {projects.map(({
+        title, linkImg, description, dateConclusion, linkApp,
+      }) => (
+        <div className="card" key={title}>
+          <img src={linkImg} alt={`Projeto ${title}`} />
+          <div className="card-info">
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <span>{`Finalizado: ${dateConclusion}`}</span>
+            <a className="button-visit" href={linkApp} target="_blank" rel="noreferrer">visitar</a>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+
   return (
     <main className="Projects">
-      <h1>Projects</h1>
+      <h1>Projetos</h1>
+      <div className="project-cards">
+        {projects !== undefined ? cards() : ''}
+      </div>
     </main>
   );
 }
