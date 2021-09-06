a2633171cdf756d12520c19f2d8f6ad003c6a054
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-06 15:46:48 -0300

    add form and title in page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 function Contact() {
   return (
     <main className="Contact">
-      <h1>Contact</h1>
+      <h1>Contato</h1>
+
+      <form action="#">
+        <input
+          type="text"
+          placeholder="Nome e Sobrenome"
+          required
+          maxLength="50"
+          minLength="3"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          required
+        />
+
+        <textarea
+          cols="40"
+          rows="4"
+          placeholder="Digite aqui sua mensagem."
+          maxLength="500"
+          required
+        />
+
+        <button type="submit" className="">Eviar</button>
+      </form>
     </main>
   );
 }
