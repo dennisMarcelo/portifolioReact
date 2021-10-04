0a01db70fbb8adc0fcf53c4f9603babc602cdd9f
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-04 18:12:52 -0300

    removed hard-skills route

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom';
 import Provider from './provider/Provider';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import HardSkills from './pages/HardSkills';
 import Projects from './pages/Projects';
 import Page404 from './pages/Page404';
 import Header from './components/Header';
@@ -17,7 +16,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/contact" component={Contact} />
-          <Route path="/hardskills" component={HardSkills} />
           <Route path="/projects" component={Projects} />
           <Route path="*" component={Page404} />
         </Switch>
