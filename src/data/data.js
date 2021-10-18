d92c8baba93496855df76219aaafd642e38aafa1
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-18 15:59:19 -0300

    recrevendo data

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export default function data() {
       imgURL: 'https://i.ibb.co/R7jqnFW/37647447.jpg',
       name: 'Dennis Marcelo',
       specialty: 'Desenvolvedor Full-Stack',
-      resume: ['Atualmente estudo na Trybe onde busco me tornar full-stack utilizando tecnologias como JavaScript, React, nodeJs, mongoDB e Python. Na Trybe também busco desenvolver habilidades  sociocomportamentais e metodologias ágeis como scrum e kanban.', 'Sou formado em análise e desenvolvimento de sistema pela universidade Estácio, apaixonado por tecnologia e pela praticidade e comodidade que elas nos proporcionam, pelo mesmo motivo decidi me tornar desenvolvedor.'],
+      resume: ['Atualmente estudo na Trybe onde busco me tornar full-stack utilizando tecnologias como JavaScript, React, nodeJs, mongoDB e Python.', 'Sou formado em análise e desenvolvimento de sistema pela universidade Estácio, apaixonado por tecnologia e pela praticidade e comodidade que elas nos proporcionam, pelo mesmo motivo decidi me tornar desenvolvedor.'],
     },
     hardSkills: {
       frontEnd: [' HTML', 'CSS', 'JavaScritp', 'React'],
