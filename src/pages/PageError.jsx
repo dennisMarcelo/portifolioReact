42b4e2f92cd843dbff1caa992b3a46013811080f
dennisMarcelo <dennismarcelo15@gmail.com>
2021-12-22 21:53:25 -0300

    added page erro and gif erro

---
+++ src/pages/PageError.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import gifErro from '../imgs/gifErro.gif';
+
+function PageError() {
+  const { message } = useParams();
+  return (
+    <div className="PageError">
+      <h1>{`Error: ${message}`}</h1>
+      <img src={gifErro} alt="imageError" />
+    </div>
+  );
+}
+
+export default PageError;
